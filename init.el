63cb11f773606baba83d695355ce62521f135bfd
looph0le <mitanshpanchal@gmail.com>
2022-01-05 04:01:42 +0530

    added config

---
+++ init.el
@@ -0,0 +1,122 @@
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package ewal
+  :init (setq ewal-use-built-in-always-p nil
+              ewal-use-built-in-on-failure-p t
+              ewal-built-in-palette "sexy-material"))
+
+(use-package ewal-spacemacs-themes
+  :if window-system
+  :ensure t
+  :init
+  (load-theme 'ewal-spacemacs-classic t)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (window-divider-mode 1))
+
+(use-package evil
+	:ensure t
+	:init
+	(setq evil-want-integreation t)
+	(setq evil-want-keybinding nil)
+	(setq evil-vsplit-window-right t)
+	(setq evil-split-window-below t)
+	(evil-mode))
+(use-package evil-collection
+	:after evil
+	:ensure t
+	:config
+	(evil-collection-init))
+
+
+(add-hook 'text-mode-hook 'visual-line-mode)
+
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+(use-package which-key
+:ensure t)
+(which-key-mode)
+
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+
+(add-to-list 'default-frame-alist
+	     '(font . "JetBrainsMono Nerd Font-10.5"))
+
+
+
+(setq use-dialog-box nil)
+(setq use-file-dialog nil)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(global-subword-mode 1)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-startup-banner "~/.emacs.d/avatar.png")
+  (setq dashboard-banner-logo-title "Welcome ladies and gentlement to looph0le's Emacs!!"))
+
+(use-package switch-window
+  :ensure t
+  :config
+  (setq switch-window-input-style 'minibuffer)
+  (setq switch-window-increase 4)
+  (setq switch-window-threshold 2)
+  (setq switch-window-shortcut-style 'qwerty)
+  (setq switch-window-qwerty-shortcuts
+	'("a" "s" "d" "f" "j" "k" "l"))
+  :bind
+  ([remap other-window] . switch-window))
+
+(use-package hungry-delete
+  :ensure t
+  :config (global-hungry-delete-mode))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package magit
+  :ensure t
+  :config
+  (setq magit-push-always-verify nil)
+  (setq git-commit-summary-max-length 50)
+  :bind
+  ("M-g" . magit-status))
+
+(defun entry-time-stamp()
+  (interactive)
+  (insert (format-time-string "<i class='ts'>%a %I:%m.%P %d.%m.%4Y</i>")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit org-bullets hungry-delete switch-window ewal-spacemacs-themes which-key use-package evil-collection doom-themes doom-modeline dashboard)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
